b129d94b00fd6dac37c37d746daaf59a192ef234
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2023-09-27 14:09:09 -0400

    fix interrupt handler to not loop indefinitely

--- disco/src/main.cpp
+++ disco/src/main.cpp
@@ -9,6 +9,7 @@
 #define BLUE_PIN 4
 
 static volatile bool keep_partying = false;
+static volatile bool needs_kill = false;
 
 void kill_party() {
 	digitalWrite(RED_PIN, LOW);
@@ -19,38 +20,23 @@ void kill_party() {
 
 void do_discotime() {
 	digitalWrite(MOTOR_PIN, HIGH);
-
 	digitalWrite(RED_PIN, HIGH);
 	digitalWrite(GREEN_PIN, HIGH);
-	digitalWrite(BLUE_PIN, HIGH);
-
-	while (keep_partying) {
-		digitalWrite(RED_PIN, LOW);
-		digitalWrite(BLUE_PIN, HIGH);
-		delay(625);
-		digitalWrite(RED_PIN, HIGH);
-		digitalWrite(GREEN_PIN, LOW);
-		delay(625);
-		digitalWrite(GREEN_PIN, HIGH);
-		digitalWrite(BLUE_PIN, LOW);
-		delay(625);
-	}
-
-	kill_party();
+	digitalWrite(BLUE_PIN, LOW);
+	keep_partying = true;
 }
 
 void handle_op(uint8_t op) {
 	switch (op) {
 		case OP_DISCOTIME:
-			keep_partying = true;
 			do_discotime();
 			break;
 		case OP_STOP_DISCOTIME:
+			needs_kill = true;
 			keep_partying = false;
 			break;
 		default:
-			Serial.print("Unknown packet op: ");
-			Serial.println(op);
+			// unsupported op
 			break;
 	}
 }
@@ -69,11 +55,27 @@ void setup() {
 
 	kill_party();
 
-	Serial.begin(9600);
 	WiFi.mode(WIFI_STA);
 	esp_now_init();
 	esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
 	esp_now_register_recv_cb(recv_callback);
 }
 
-void loop() {}
+void loop() {
+	while (keep_partying) {
+		digitalWrite(RED_PIN, LOW);
+		digitalWrite(BLUE_PIN, HIGH);
+		delay(625);
+		digitalWrite(RED_PIN, HIGH);
+		digitalWrite(GREEN_PIN, LOW);
+		delay(625);
+		digitalWrite(GREEN_PIN, HIGH);
+		digitalWrite(BLUE_PIN, LOW);
+		delay(625);
+	}
+
+	if (needs_kill) {
+		kill_party();
+		needs_kill = false;
+	}
+}
