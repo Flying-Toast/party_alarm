4d956850075a61506e381140154303c670bb6568
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2023-09-27 13:57:25 -0400

    fix disco mac

--- alarm/src/main.cpp
+++ alarm/src/main.cpp
@@ -5,15 +5,15 @@
 // this is a weak symbol in esp8266ArduinoCore code, override it so we save time by not disabling WiFi
 extern "C" void __disableWiFiAtBootTime(void) {}
 
-static uint8_t lightswitch_mac[6] = {0x84,0xf3,0xeb,0xb2,0x32,0x9e};
+static uint8_t disco_mac[6] = {0xa4,0xe5,0x7c,0xbc,0xdb,0xe9};
 
 extern "C" void setup() {
 	esp_now_init();
 	esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-	esp_now_add_peer(lightswitch_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+	esp_now_add_peer(disco_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 
-	uint8_t ops[] = {OP_LIGHTS_OFF, OP_DISCOTIME, OP_PLAY_MUSIC};
-	esp_now_send(lightswitch_mac, ops, sizeof(ops));
+	uint8_t op = OP_DISCOTIME;
+	esp_now_send(disco_mac, &op, /* length: */ 1);
 }
 
 extern "C" void loop() {
