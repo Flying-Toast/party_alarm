192100a943b099a3268d79bd8bc8d75bbabf91be
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2023-10-11 22:07:03 -0400

    add speaker as slave

--- alarm/src/main.cpp
+++ alarm/src/main.cpp
@@ -29,6 +29,7 @@ extern "C" void setup() {
 	esp_now_init();
 	esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
 	esp_now_add_peer(disco_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+	esp_now_add_peer(speaker_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 
 	pinMode(PARTY_STOP_PIN, INPUT);
 	if (digitalRead(PARTY_STOP_PIN)) {
