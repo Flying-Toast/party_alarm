994161f6ee21b7ced435ab1fa875af80d51492f6
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2023-10-11 22:09:48 -0400

    WHOOOPS

--- shared_defs.h
+++ shared_defs.h
@@ -1,11 +1,13 @@
 #ifndef __HAVE_SHARED_DEFS_H
 #define __HAVE_SHARED_DEFS_H
 
+// NOTE: If you change the order of these,
+// make sure you re-upload to slaves
 enum packet_op {
 	OP_LIGHTS_OFF = 0,
 	OP_DISCOTIME,
-	OP_STOP_DISCOTIME,
 	OP_PLAYPAUSE_MUSIC,
+	OP_STOP_DISCOTIME,
 };
 
 #endif
