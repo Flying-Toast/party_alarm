b550a5c9da451e478eeb5b7d5b4bd7dccedd0086
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2023-10-14 19:07:24 -0400

    speaker monitors playback

--- speaker/src/main.cpp
+++ speaker/src/main.cpp
@@ -5,27 +5,30 @@
 
 #define PLAYPAUSE_PIN D7
 #define AMP_POW_PIN D8
+#define PLAYBACK_MON_PIN D2
 
-static volatile bool playing = false;
+static volatile bool amp_powered = false;
+
+IRAM_ATTR void playback_change() {
+	amp_powered = !amp_powered;
+	digitalWrite(AMP_POW_PIN, amp_powered);
+}
 
 void playpause()
 {
 	digitalWrite(PLAYPAUSE_PIN, HIGH);
 	delay(100);
 	digitalWrite(PLAYPAUSE_PIN, LOW);
-
-	playing = !playing;
-	digitalWrite(AMP_POW_PIN, playing);
 }
 
 void handle_op(uint8_t op) {
 	switch (op) {
 		case OP_PLAY_MUSIC:
-			if (!playing)
+			if (!amp_powered)
 				playpause();
 			break;
 		case OP_PAUSE_MUSIC:
-			if (playing)
+			if (amp_powered)
 				playpause();
 			break;
 		default:
@@ -43,8 +46,16 @@ void recv_callback(uint8_t *mac, uint8_t *data, uint8_t len) {
 void setup() {
 	pinMode(PLAYPAUSE_PIN, OUTPUT);
 	pinMode(AMP_POW_PIN, OUTPUT);
+
 	digitalWrite(PLAYPAUSE_PIN, LOW);
 	digitalWrite(AMP_POW_PIN, LOW);
+
+	attachInterrupt(
+		digitalPinToInterrupt(PLAYBACK_MON_PIN),
+		playback_change,
+		CHANGE
+	);
+
 
 	WiFi.mode(WIFI_STA);
 	esp_now_init();
